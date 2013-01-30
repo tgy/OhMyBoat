590e703fd800def217e038d87a5f93aaf1a0bb25
Neodyblue <neodyblue@gmail.com>
2013-01-30 14:19:43 +0100

    packet

---
+++ OhMyBoat/OhMyBoat/OhMyBoat/Network/Packet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OhMyBoat.IO;
+
+namespace OhMyBoat.Network
+{
+    public class Packet
+    {
+        public const byte HeaderSize = 2; // un byte opcode et un byte de taille
+
+        public static PacketHeader GetHeader(Reader r)
+        {
+            return new PacketHeader(r.ReadByte(), r.ReadByte());
+        }
+    }
+}
